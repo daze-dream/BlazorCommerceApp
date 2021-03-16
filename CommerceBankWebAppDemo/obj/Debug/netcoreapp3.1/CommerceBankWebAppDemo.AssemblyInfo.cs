7a0e3f5973b4ef0efb0197711f94cf3af64cea4c
CheetoDustNeo <misterrolypoly@gmail.com>
2021-03-15 22:19:49 -0500

    added Azure connection string to solution

--- CommerceBankWebAppDemo/obj/Debug/netcoreapp3.1/CommerceBankWebAppDemo.AssemblyInfo.cs
+++ CommerceBankWebAppDemo/obj/Debug/netcoreapp3.1/CommerceBankWebAppDemo.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("07f7978e-bbcd-4560-ba48-f8329aa8d57d")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("CommerceBankWebAppDemo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
