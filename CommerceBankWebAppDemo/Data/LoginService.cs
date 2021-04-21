e50a3772b08cdd431866bf4f31bc21f24120d489
Sandy <syy6v@mail.umkc.edu>
2021-04-20 20:40:44 -0500

    Added Notifications Page. Made some changes to Transactions Page (validation).

---
+++ CommerceBankWebAppDemo/Data/LoginService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CommerceBankWebAppDemo.Data.Commerce;
+using Microsoft.EntityFrameworkCore;
+
+namespace CommerceBankWebAppDemo.Data
+{
+    public class LoginService
+    {
+        private readonly CommerceappContext _context;
+
+        public LoginService(CommerceappContext context)
+        {
+            _context = context;
+        }
+    }
+}
