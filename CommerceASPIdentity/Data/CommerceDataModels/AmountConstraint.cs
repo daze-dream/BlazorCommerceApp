400eb0d34de7e39a99678dbe34ef225220629741
trevorjpt33 <33333712+trevorjpt33@users.noreply.github.com>
2021-04-26 13:28:26 -0500

    Newest Updates

--- CommerceASPIdentity/Data/CommerceDataModels/AmountConstraint.cs
+++ CommerceASPIdentity/Data/CommerceDataModels/AmountConstraint.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 #nullable disable
 
