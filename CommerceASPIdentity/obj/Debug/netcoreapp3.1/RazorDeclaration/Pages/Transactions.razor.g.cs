769a93c606951d2f88578a4fb22174865c854e52
trevorjpt33 <33333712+trevorjpt33@users.noreply.github.com>
2021-04-26 15:04:55 -0500

    Newest updates with updated redirection progress

--- CommerceASPIdentity/obj/Debug/netcoreapp3.1/RazorDeclaration/Pages/Transactions.razor.g.cs
+++ CommerceASPIdentity/obj/Debug/netcoreapp3.1/RazorDeclaration/Pages/Transactions.razor.g.cs
@@ -13,98 +13,98 @@ namespace EndToEndTest.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\Sandy\Desktop\Capstone\semester-project-group-5-commerce\CommerceASPIdentity\_Imports.razor"
+#line 1 "D:\Users\Trevor\Google Drive\School\Spring 2021\CS456\semester-project-group-5-commerce\CommerceASPIdentity\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\Sandy\Desktop\Capstone\semester-project-group-5-commerce\CommerceASPIdentity\_Imports.razor"
+#line 2 "D:\Users\Trevor\Google Drive\School\Spring 2021\CS456\semester-project-group-5-commerce\CommerceASPIdentity\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\Sandy\Desktop\Capstone\semester-project-group-5-commerce\CommerceASPIdentity\_Imports.razor"
+#line 3 "D:\Users\Trevor\Google Drive\School\Spring 2021\CS456\semester-project-group-5-commerce\CommerceASPIdentity\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\Sandy\Desktop\Capstone\semester-project-group-5-commerce\CommerceASPIdentity\_Imports.razor"
+#line 5 "D:\Users\Trevor\Google Drive\School\Spring 2021\CS456\semester-project-group-5-commerce\CommerceASPIdentity\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\Sandy\Desktop\Capstone\semester-project-group-5-commerce\CommerceASPIdentity\_Imports.razor"
+#line 6 "D:\Users\Trevor\Google Drive\School\Spring 2021\CS456\semester-project-group-5-commerce\CommerceASPIdentity\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\Sandy\Desktop\Capstone\semester-project-group-5-commerce\CommerceASPIdentity\_Imports.razor"
+#line 7 "D:\Users\Trevor\Google Drive\School\Spring 2021\CS456\semester-project-group-5-commerce\CommerceASPIdentity\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\Sandy\Desktop\Capstone\semester-project-group-5-commerce\CommerceASPIdentity\_Imports.razor"
+#line 8 "D:\Users\Trevor\Google Drive\School\Spring 2021\CS456\semester-project-group-5-commerce\CommerceASPIdentity\_Imports.razor"
 using EndToEndTest;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\Sandy\Desktop\Capstone\semester-project-group-5-commerce\CommerceASPIdentity\_Imports.razor"
+#line 9 "D:\Users\Trevor\Google Drive\School\Spring 2021\CS456\semester-project-group-5-commerce\CommerceASPIdentity\_Imports.razor"
 using EndToEndTest.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\Sandy\Desktop\Capstone\semester-project-group-5-commerce\CommerceASPIdentity\Pages\Transactions.razor"
+#line 2 "D:\Users\Trevor\Google Drive\School\Spring 2021\CS456\semester-project-group-5-commerce\CommerceASPIdentity\Pages\Transactions.razor"
 using EndToEndTest.Data.CommerceDataModels;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\Sandy\Desktop\Capstone\semester-project-group-5-commerce\CommerceASPIdentity\Pages\Transactions.razor"
+#line 3 "D:\Users\Trevor\Google Drive\School\Spring 2021\CS456\semester-project-group-5-commerce\CommerceASPIdentity\Pages\Transactions.razor"
 using Data;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\Sandy\Desktop\Capstone\semester-project-group-5-commerce\CommerceASPIdentity\Pages\Transactions.razor"
+#line 4 "D:\Users\Trevor\Google Drive\School\Spring 2021\CS456\semester-project-group-5-commerce\CommerceASPIdentity\Pages\Transactions.razor"
 using System.Security.Claims;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\Sandy\Desktop\Capstone\semester-project-group-5-commerce\CommerceASPIdentity\Pages\Transactions.razor"
+#line 5 "D:\Users\Trevor\Google Drive\School\Spring 2021\CS456\semester-project-group-5-commerce\CommerceASPIdentity\Pages\Transactions.razor"
 using Microsoft.AspNetCore.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\Sandy\Desktop\Capstone\semester-project-group-5-commerce\CommerceASPIdentity\Pages\Transactions.razor"
+#line 6 "D:\Users\Trevor\Google Drive\School\Spring 2021\CS456\semester-project-group-5-commerce\CommerceASPIdentity\Pages\Transactions.razor"
 using Microsoft.Data.SqlClient;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\Sandy\Desktop\Capstone\semester-project-group-5-commerce\CommerceASPIdentity\Pages\Transactions.razor"
+#line 7 "D:\Users\Trevor\Google Drive\School\Spring 2021\CS456\semester-project-group-5-commerce\CommerceASPIdentity\Pages\Transactions.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
@@ -119,7 +119,7 @@ using Microsoft.AspNetCore.Components.Forms;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 199 "C:\Users\Sandy\Desktop\Capstone\semester-project-group-5-commerce\CommerceASPIdentity\Pages\Transactions.razor"
+#line 199 "D:\Users\Trevor\Google Drive\School\Spring 2021\CS456\semester-project-group-5-commerce\CommerceASPIdentity\Pages\Transactions.razor"
        
     [CascadingParameter]
     private Task<AuthenticationState> authenticationStateTask { get; set; }
